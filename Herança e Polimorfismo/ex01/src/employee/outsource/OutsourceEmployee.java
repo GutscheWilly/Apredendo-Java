0353b0fa49da7dc788e1472c304d32de136dc0a0
Willy Gutsche <willy.gutsche@unifesp.br>
2022-11-14 00:25:34 -0300

    Class: OutsourceEmployee

---
+++ Herança e Polimorfismo/ex01/src/employee/outsource/OutsourceEmployee.java
@@ -0,0 +1,26 @@
+package employee.outsource;
+
+import employee.Employee;
+
+public class OutsourceEmployee extends Employee{
+    
+    private Double addicionalCharge;
+
+    public OutsourceEmployee(String name, Integer hours, Double valuePerHour, Double addicionalCharge) {
+        super(name, hours, valuePerHour);
+        this.addicionalCharge = addicionalCharge;
+    }
+
+    public Double getAddicionalCharge() {
+        return addicionalCharge;
+    }
+
+    public void setAddicionalCharge(Double addicionalCharge) {
+        this.addicionalCharge = addicionalCharge;
+    }
+
+    @Override
+    public final Double payment() {
+        return super.payment() + (1.1 * addicionalCharge);
+    }
+}
