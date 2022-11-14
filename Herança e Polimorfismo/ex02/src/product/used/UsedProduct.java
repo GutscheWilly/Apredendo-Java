5bbd9f183687a4485c1644af5c0d2055cf1b1596
Willy Gutsche <willy.gutsche@unifesp.br>
2022-11-14 20:17:25 -0300

    Class: UsedProduct

---
+++ Herança e Polimorfismo/ex02/src/product/used/UsedProduct.java
@@ -0,0 +1,24 @@
+package product.used;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import product.Product;
+
+public class UsedProduct extends Product {
+
+    private LocalDate manufactureDate;
+
+    public UsedProduct(String name, Double price, LocalDate manufactureDate) {
+        super(name, price);
+        this.manufactureDate = manufactureDate;
+    }
+
+    @Override
+    public String priceTag() {
+        return String.format("%s (used) - $ %.2f (Manufacture date: %s)", 
+        getName(),
+        getPrice(),
+        manufactureDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+    }
+}
