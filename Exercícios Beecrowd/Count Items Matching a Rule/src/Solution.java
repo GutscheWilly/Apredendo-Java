28b280720dc3fb596e2707a37ad466d5c9fce4d4
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-19 00:38:52 -0300

    Improve solution

--- Exercícios Beecrowd/Count Items Matching a Rule/src/Solution.java
+++ Exercícios Beecrowd/Count Items Matching a Rule/src/Solution.java
@@ -4,21 +4,15 @@ import java.util.Map;
 
 public class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-        int numberOfItemsMatches = 0;
-        int ruleKeyIndex = itemMap.get(ruleKey);
+        final int ruleKeyIndex = itemMap.get(ruleKey);
 
-        for (List<String> item : items) {
-            String attribute = item.get(ruleKeyIndex);
-
-            if (attribute.equals(ruleValue)) {
-                numberOfItemsMatches++;
-            }
-        }
-
-        return numberOfItemsMatches;
+        return (int) items.stream()
+                .map(item -> item.get(ruleKeyIndex))
+                .filter(attribute -> attribute.equals(ruleValue))
+                .count();
     }
 
-    public static Map<String, Integer> itemMap = new HashMap<>() {
+    public final Map<String, Integer> itemMap = new HashMap<>() {
         {
             put("type", 0);
             put("color", 1);
